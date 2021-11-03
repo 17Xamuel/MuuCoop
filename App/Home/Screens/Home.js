4a2630c9754c224d5377a30991c5d6daf19cd3c5
XamuelUG <samweko@gmail.com>
2021-11-03 12:07:08 +0300

    finishing screens

--- App/Home/Screens/Home.js
+++ App/Home/Screens/Home.js
@@ -1,32 +1,287 @@
 import React, {Component} from 'react';
 
-import {View, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableWithoutFeedback,
+  Touchable,
+  TouchableOpacity,
+} from 'react-native';
+
+import Feather from 'react-native-vector-icons/Feather';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import * as Animatable from 'react-native-animatable';
+import {TextInput} from 'react-native-paper';
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      saveInputCtr: false,
+      loanInputCtr: false,
+    };
   }
   render() {
     return (
       <>
-        <View style={{flex: 1, backgroundColor: '#005CE6'}}>
-          <View style={styles.topCtr}></View>
+        <View
+          style={{flex: 1, backgroundColor: '#005CE6', position: 'relative'}}>
+          <View style={styles.topCtr}>
+            <View>
+              <View
+                style={{
+                  marginTop: 20,
+                  flexDirection: 'row',
+                  // alignItems: 'center',
+                }}>
+                <Image
+                  source={require('../../Resources/place-holder.jpg')}
+                  style={{height: 60, width: 60, borderRadius: 30}}
+                />
+                <View style={{marginLeft: 20}}>
+                  <Text style={{color: 'white', fontSize: 18.5}}>
+                    Owobushobozi Derrick
+                  </Text>
+                  <Text style={{color: 'white'}}>Member Number: 31</Text>
+                </View>
+              </View>
+            </View>
+          </View>
           <View style={styles.bottomCtr}>
             <View style={styles.bottom}>
-              <View style={styles.summuryCtr}>
-                <Text>Styling</Text>
+              <View style={[styles.summuryCtr, styles.shadow]}>
+                <Text style={{opacity: 0.5, fontSize: 13}}>MUU Coop</Text>
+                <Text style={{fontSize: 16}}>Savings</Text>
+                <Text style={{fontSize: 16}}>Loans</Text>
+                <Text style={{fontSize: 16}}>Mutuals</Text>
               </View>
               <View style={styles.saveAndLoansCtr}>
                 <View style={styles.saveCtr}>
-                  <View style={styles.saveLeft}>
-                    <Text>Styling</Text>
+                  <View
+                    style={[
+                      {
+                        flex: 1.5,
+                        backgroundColor: 'white',
+                        marginRight: 5,
+                        borderRadius: 5,
+                        justifyContent: 'space-around',
+                        paddingLeft: 15,
+                      },
+                      styles.shadow,
+                    ]}>
+                    <FontAwesome5 name="wallet" size={40} color="#000" />
+                    <View>
+                      <Text style={{fontSize: 13}}>My Savings</Text>
+                      <Text style={{fontSize: 18.5, fontWeight: 'bold'}}>
+                        UGX 5000
+                      </Text>
+                    </View>
                   </View>
-                  <View style={styles.saveRight}></View>
+                  <View
+                    style={{
+                      flex: 1.5,
+                      marginLeft: 5,
+                      justifyContent: 'space-around',
+                      paddingLeft: 20,
+                    }}>
+                    <Text style={{fontSize: 15, fontWeight: 'bold'}}>
+                      Model Uganda Cooperation
+                    </Text>
+                    <Text>Members: 35</Text>
+                    <Text>Members: 35</Text>
+                    <Text>Mutual Loans: 3</Text>
+                    <Text>Personal Loans: 5</Text>
+                  </View>
                 </View>
-                <View style={styles.loansCtr}></View>
+                <View style={styles.loansCtr}>
+                  <View
+                    style={{
+                      flex: 2,
+                      marginRight: 5,
+                    }}>
+                    <View
+                      style={[
+                        {
+                          flex: 1,
+                          marginBottom: 2.5,
+                          borderRadius: 5,
+                          flexDirection: 'row',
+                          justifyContent: 'space-around',
+                          alignItems: 'center',
+                        },
+                        styles.shadow,
+                      ]}>
+                      <View>
+                        <Text style={{fontSize: 12}}>Total Savings</Text>
+                        <Text style={{fontSize: 16, fontWeight: 'bold'}}>
+                          UGX 6,780,000
+                        </Text>
+                      </View>
+                      <FontAwesome5 name="wallet" size={25} color="#000" />
+                    </View>
+                    <View
+                      style={{
+                        flex: 1,
+                        marginTop: 2.5,
+                        borderRadius: 5,
+                        justifyContent: 'space-around',
+                      }}>
+                      <View>
+                        <Text
+                          style={{
+                            fontSize: 18,
+                            fontWeight: 'bold',
+                            paddingLeft: 25,
+                          }}>
+                          Actions
+                        </Text>
+                      </View>
+                      <View
+                        style={{
+                          flexDirection: 'row',
+                          justifyContent: 'space-between',
+                        }}>
+                        <TouchableOpacity
+                          style={[styles.stepButton]}
+                          onPress={() => {
+                            this.setState({
+                              ...this.state,
+                              saveInputCtr: true,
+                            });
+                          }}>
+                          <Text
+                            style={{
+                              fontSize: 13,
+                              color: '#fff',
+                            }}>
+                            Save
+                          </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                          style={[styles.stepButton]}
+                          onPress={() => {
+                            this.setState({
+                              ...this.state,
+                              loanInputCtr: true,
+                            });
+                          }}>
+                          <Text
+                            style={{
+                              fontSize: 13,
+                              color: '#fff',
+                            }}>
+                            Get a Loan
+                          </Text>
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                  </View>
+                  <View
+                    style={[
+                      {
+                        flex: 1.5,
+                        marginLeft: 5,
+                        borderRadius: 5,
+                        justifyContent: 'space-around',
+                        paddingLeft: 15,
+                      },
+                      styles.shadow,
+                    ]}>
+                    <FontAwesome5
+                      name="money-check-alt"
+                      size={30}
+                      color="#000"
+                    />
+                    <View>
+                      <Text style={{fontSize: 13}}>Total Loan Balance</Text>
+                      <Text style={{fontSize: 18.5, fontWeight: 'bold'}}>
+                        UGX 1500
+                      </Text>
+                    </View>
+                  </View>
+                </View>
               </View>
             </View>
+          </View>
+          <View
+            style={[
+              styles.login,
+              {
+                display:
+                  this.state.saveInputCtr || this.state.loanInputCtr
+                    ? 'flex'
+                    : 'none',
+              },
+            ]}
+            animation="fadeInUpBig">
+            <View style={{position: 'absolute', left: 5, top: 5}}>
+              <FontAwesome5
+                name="times"
+                size={15}
+                onPress={() => {
+                  this.setState({
+                    ...this.state,
+                    saveInputCtr: false,
+                    loanInputCtr: false,
+                  });
+                }}
+              />
+            </View>
+            <View>
+              <Text style={{fontSize: 16, fontWeight: 'bold'}}>
+                {this.state.saveInputCtr ? 'Make A Saving' : ''}
+                {this.state.loanInputCtr ? 'Loan Request Details' : ''}
+              </Text>
+            </View>
+            <View>
+              <TextInput
+                style={styles.input_ctr}
+                label="Transaction Amount"
+                mode="outlined"
+                onChangeText={e => {
+                  this.setState({...this.state, amount: e});
+                }}
+                right={
+                  <TextInput.Icon
+                    name={this.state.passwordVisible ? 'eye-off' : 'eye'}
+                  />
+                }
+              />
+              <TextInput
+                style={styles.input_ctr}
+                label="Mobile Number"
+                mode="outlined"
+                onChangeText={e => {
+                  this.setState({...this.state, number: e});
+                }}
+                right={
+                  <TextInput.Icon
+                    name={this.state.passwordVisible ? 'eye-off' : 'eye'}
+                  />
+                }
+              />
+            </View>
+            <TouchableOpacity style={styles.stepButton}>
+              <Text
+                style={{
+                  fontSize: 20,
+                  color: '#fff',
+                  paddingBottom: 5,
+                  marginRight: 15,
+                }}>
+                {this.state.saveInputCtr ? 'Save' : ''}
+                {this.state.loanInputCtr ? 'Send Loan Request' : ''}
+              </Text>
+              <Feather name="chevron-right" size={18} color="#fff" />
+            </TouchableOpacity>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}></View>
           </View>
         </View>
       </>
@@ -39,13 +294,13 @@ export default Home;
 const styles = StyleSheet.create({
   topCtr: {
     flex: 1,
+    marginHorizontal: 25,
   },
   bottomCtr: {
     flex: 2.5,
     backgroundColor: '#fff',
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
-    // position: 'relative',
   },
   bottom: {
     marginHorizontal: 25,
@@ -55,27 +310,64 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 15,
-    top: -20,
+    top: -30,
   },
   summuryCtr: {
-    flex: 1,
-    backgroundColor: 'blue',
+    flex: 0.8,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
     borderRadius: 5,
   },
   saveAndLoansCtr: {
-    flex: 3,
+    flex: 4.5,
     marginVertical: 5,
   },
   saveCtr: {
-    backgroundColor: 'red',
     flex: 1,
     borderRadius: 5,
     marginVertical: 5,
+    flexDirection: 'row',
+  },
+  input_ctr: {
+    marginBottom: 32,
+    marginTop: 10,
+  },
+  login: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    top: 32,
+    zIndex: 5,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
   },
   loansCtr: {
-    backgroundColor: 'black',
     flex: 1,
     borderRadius: 5,
     marginVertical: 5,
+    flexDirection: 'row',
+  },
+  stepButton: {
+    borderRadius: 30,
+    backgroundColor: '#005CE6',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 5,
+    marginVertical: 5,
+    paddingHorizontal: 15,
+  },
+  shadow: {
+    shadowColor: 'rgba(0,0,0,0.5)',
+    // shadowOffset: {width: 1, height: 1},
+    shadowOpacity: 2,
+    shadowRadius: 2,
+    elevation: 2.5,
   },
 });
--- App/Home/Screens/Mutual.js
+++ App/Home/Screens/Mutual.js
@@ -1,17 +1,159 @@
 import React, {Component} from 'react';
 
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  FlatList,
+  ToastAndroid,
+} from 'react-native';
+
+//input ctr
+import Feather from 'react-native-vector-icons/Feather';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import {TextInput} from 'react-native-paper';
 
 class MutualAccount extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {loanInputCtr: false};
   }
+
+  renderItem = () => <MutualLoan />;
+
   render() {
     return (
       <>
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <Text>Mutual Account</Text>
+        <View style={{flex: 1}}>
+          <View style={styles.topCtr}>
+            <View>
+              <View
+                style={{
+                  marginTop: 20,
+                  flexDirection: 'row',
+                  // alignItems: 'center',
+                }}>
+                <Image
+                  source={require('../../Resources/place-holder.jpg')}
+                  style={{height: 60, width: 60, borderRadius: 30}}
+                />
+                <View style={{marginLeft: 20}}>
+                  <Text style={{color: 'white', fontSize: 15}}>
+                    Owobushobozi Derrick
+                  </Text>
+                  <Text style={{color: 'white'}}>Member Number: 31</Text>
+                </View>
+              </View>
+              <View>
+                <Text
+                  style={{color: 'white', fontSize: 30, fontWeight: 'bold'}}>
+                  Mutual Loans
+                </Text>
+              </View>
+              <View>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => {
+                    this.setState({
+                      ...this.state,
+                      loanInputCtr: true,
+                    });
+                  }}>
+                  <Text>Get A Loan</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+          <View style={styles.bottomCtr}>
+            <View style={{marginHorizontal: 20, marginVertical: 10}}>
+              <Text style={{fontWeight: 'bold', fontSize: 18.5}}>
+                My Mutual Loans
+              </Text>
+            </View>
+            <View>
+              <FlatList
+                data={DATA}
+                renderItem={this.renderItem}
+                keyExtractor={item => item.id}
+                contentContainerStyle={{paddingBottom: 40}}
+              />
+            </View>
+          </View>
+          <View
+            style={[
+              styles.login,
+              {
+                display: this.state.loanInputCtr ? 'flex' : 'none',
+              },
+            ]}
+            animation="fadeInUpBig">
+            <View style={{position: 'absolute', left: 5, top: 5}}>
+              <FontAwesome5
+                name="times"
+                size={15}
+                onPress={() => {
+                  this.setState({
+                    ...this.state,
+                    loanInputCtr: false,
+                  });
+                }}
+              />
+            </View>
+            <View>
+              <Text style={{fontSize: 14, fontWeight: 'bold'}}>
+                {this.state.loanInputCtr ? 'Mutual Loan Request' : ''}
+              </Text>
+            </View>
+            <View>
+              <TextInput
+                style={styles.input_ctr}
+                label="Transaction Amount"
+                mode="outlined"
+                onChangeText={e => {
+                  this.setState({...this.state, amount: e});
+                }}
+                right={
+                  <TextInput.Icon
+                    name={this.state.passwordVisible ? 'eye-off' : 'eye'}
+                  />
+                }
+              />
+              <TextInput
+                style={styles.input_ctr}
+                label="Mobile Number"
+                mode="outlined"
+                onChangeText={e => {
+                  this.setState({...this.state, number: e});
+                }}
+                right={
+                  <TextInput.Icon
+                    name={this.state.passwordVisible ? 'eye-off' : 'eye'}
+                  />
+                }
+              />
+            </View>
+            <TouchableOpacity style={styles.buttonNoWidth}>
+              <Text
+                style={{
+                  fontSize: 20,
+                  color: '#fff',
+                  paddingBottom: 5,
+                  marginRight: 15,
+                }}>
+                {this.state.loanInputCtr ? 'Send Loan Request' : ''}
+              </Text>
+              <Feather name="chevron-right" size={18} color="#fff" />
+            </TouchableOpacity>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}></View>
+          </View>
         </View>
       </>
     );
@@ -19,3 +161,112 @@ class MutualAccount extends Component {
 }
 
 export default MutualAccount;
+
+const styles = StyleSheet.create({
+  bottomCtr: {
+    flex: 2,
+  },
+  button: {
+    width: Dimensions.get('screen').width * 0.32,
+    borderRadius: 30,
+    backgroundColor: '#9f9f9f',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 5,
+    marginVertical: 5,
+    paddingHorizontal: 15,
+  },
+  buttonNoWidth: {
+    borderRadius: 30,
+    backgroundColor: '#005CE6',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 5,
+    marginVertical: 5,
+    paddingHorizontal: 15,
+  },
+  input_ctr: {
+    marginBottom: 32,
+    marginTop: 10,
+  },
+  login: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    top: 32,
+    zIndex: 5,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  topCtr: {
+    flex: 1,
+    backgroundColor: '#005CE6',
+    paddingHorizontal: 25,
+    borderBottomRightRadius: 15,
+    borderBottomLeftRadius: 15,
+  },
+});
+
+//flatlist component
+const DATA = [
+  {
+    id: '1',
+    user: 'Xamuel',
+  },
+  {
+    id: '2',
+    user: 'Xamuel2',
+  },
+  {
+    id: '3',
+    user: 'Xamuel3',
+  },
+  {
+    id: '4',
+    user: 'Xamuel3',
+  },
+  {
+    id: '5',
+    user: 'Xamuel3',
+  },
+  {
+    id: '6',
+    user: 'Xamuel3',
+  },
+];
+function MutualLoan() {
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        borderBottomWidth: 1,
+        borderBottomColor: 'rgba(0,0,0,0.2)',
+        paddingVertical: 10,
+      }}>
+      <View>
+        <Text>Loan Amount: 50000</Text>
+        <Text>Paid: 30000</Text>
+        <Text>Balance: 20000</Text>
+      </View>
+      <View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            ToastAndroid.show('App in Beta Version', ToastAndroid.LONG);
+          }}>
+          <Text>Details</Text>
+        </TouchableOpacity>
+        <View>
+          <Text>Guarantors: 30,4,8</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
